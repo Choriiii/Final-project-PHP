4406c75a6ccb94a569863caaf14b155d774a1a91
Jessie <jessie.2006.14@gmail.com>
2025-08-27 17:12:58 -0700

    Add some changes

--- index.php
+++ index.php
@@ -26,37 +26,129 @@ $products = [
         "image" => "https://assets.tmecosys.com/image/upload/t_web_rdp_recipe_584x480/img/recipe/ras/Assets/C07AE049-11C3-4672-A96A-A547C15F0116/Derivates/FE1D05A4-0A44-4007-9A42-5CAFD9F8F798.jpg"
     ]
 ];
-function addNewProduct(string $name, string $description, float $price){
-    $products_file = fopen("new_products.txt","a") or die ("Unable to open file!");
+    
+function addNewProduct(string $name, string $description, float $price,array $image){
+    try {
+    if (!isset($image) || $image['error'] !== UPLOAD_ERR_OK) {//UPLOAD_ERR_OK es una constante de PHP que significa 0, es decir, la subida fue exitosa.
+            throw new Exception("Error to upload the image.");
+        }
+
+    //data for the image
+    $uploadDir = 'uploads/';
+
+//Cuando subimos un archivo en PHP, el array $_FILES['image'] tiene varios elementos:
+
+    $fileName = basename($image['name']); // name → nombre original del archivo.
+        $fileTmp  = $image['tmp_name']; // tmp_name → ruta temporal en el servidor.
+        $fileSize = $image['size'];  // size → tamaño del archivo.
+        $fileType = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));//strtolower convierte las letras en minusculas -- 
+        // pathinfo es una funcion de PHP que extra info de la ruta de la imagen o nombre del archivo Puede devolver varias partes:
+
+// dirname → ruta del directorio.
+
+// basename → nombre del archivo con extensión.
+
+// filename → nombre del archivo sin extensión.
+
+// extension → extensión del archivo.
+
+// PATHINFO_EXTENSION le indica a pathinfo() que solo queremos la extensión del archivo.
+    // error → código de error de la subida.
+     $allowedTypes = ['jpg', 'jpeg', 'png', 'gif'];
+        if (!in_array($fileType, $allowedTypes)) {
+            throw new Exception("not allowed type of image. only JPG, JPEG, PNG or GIF.");
+        }
+
+        if ($fileSize > 5 * 1024 * 1024) {//1 kilobyte (KB) = 1024 bytes
+        // 1 megabyte (MB) = 1024 KB = 1024 × 1024 bytes = 1,048,576 bytes
+            throw new Exception("the image its too large. Máx 5MB.");
+        }
+        $newFileName = uniqid('img_') . '.' . $fileType;
+        $destination = $uploadDir . $newFileName;
+
+        if (!move_uploaded_file($fileTmp, $destination)) {
+            throw new Exception("unable to save the image.");
+        }
+        $products_file = fopen("new_products.txt","a") or die ("Unable to open file!");
+    // data for the audit record
     $new_product = [
         "name" => $name,
         "description" => $description,
-        "price" => $price
+        "price" => $price,
+        "image" => $destination 
     ];
-
-    $jsonData = json_encode($new_product, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
 
     $timestamp = date("Y-m-d H:i:s");
     $ip = $_SERVER['REMOTE_ADDR'];
-    $userAgent = $_SERVER['HTTP_USER_AGENT'] ;
+    $userAgent = $_SERVER['HTTP_USER_AGENT'];
+    require("./config.php");
+    $dbcon = new mysqli(DB_SERVERNAME, DB_USERNAME, DB_PASS, DB_NAME);
+            if($dbcon->connect_error){
+                throw new Exception("DB error", 500);
+            }
+             $name = $new_product["name"];
+            $description = $new_product["description"];
+            $price = $new_product["price"];
+            $image = $new_product["image"];
 
-    fwrite($products_file, "[$timestamp] - IP: $ip - User Agent: $userAgent - $jsonData" . PHP_EOL);
-    fclose($products_file);
+            $insertPrep = $dbcon->prepare("INSERT INTO products (name, description, price, image) VALUES (?, ?, ?, ?)");
+            $insertPrep->bind_param("ssds", $name, $description, $price, $image);
+            $insertPrep->execute();
+            $insertPrep->close();
+
+            // Insert en audit_record
+            $action = "added: " . $new_product["name"];
+            $insertPrep = $dbcon->prepare("INSERT INTO audit_record (timestamp, ip, userAgent, action) VALUES (?, ?, ?, ?)");
+            $insertPrep->bind_param("ssss", $timestamp, $ip, $userAgent, $action);
+            $insertPrep->execute();
+
+            $insertPrep->close();
+            $dbcon->close();
+
+    }catch(Exception $err){
+    echo $err->getMessage();
+    http_response_code($err->getCode());
+}
     }
 function displayNewProducts(){
-    if (!file_exists("new_products.txt") || filesize("new_products.txt"=== 0)){
+    if (!file_exists("new_products.txt") || filesize("new_products.txt")=== 0){
         return [];
     }else{
         $open_new_products = fopen("new_products.txt","r") or die ("Unable to open file");
         $new_products = fread($open_new_products,filesize("new_products.txt"));
         fclose($open_new_products);
+
+         $lines = explode(PHP_EOL, trim($new_products));//explode sirve para dividir una string usando un delimitador y se usa asi explode(string $delimitador, string $texto, int $limite = PHP_INT_MAX): array
+
+        $products = [];
+        foreach ($lines as $line) {
+            if (trim($line) === "") continue;
+            $parts = explode(" - ", $line, 4);
+            if (isset($parts[3])){
+                $json = $parts[3];
+                $productData = json_decode($json, true);
+                if ($productData) {
+                    $products[] = $productData;
+                }
+            }
+        }
+        return $products;
     }
     
 }
-displayNewProducts();
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    addNewProduct($_POST['name'], $_POST['description'], floatval($_POST['price']));
+function deleteProduct(){
+   
+
 }
+function editProduct(string $name, string $description, float $price,array $image){
+    
+
+}
+// calling functions
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    addNewProduct($_POST['name'], $_POST['description'], floatval($_POST['price']), $_FILES['image'] );
+}
+$new_products=displayNewProducts();
 
 ?>
 <!DOCTYPE html>
@@ -125,6 +217,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         <h3><?= $product['name'] ?></h3>
         <p><?= $product['description'] ?></p>
         <div class="price">$<?= $product['price'] ?></div>
+        <button >delete</button>
     </div>
 <?php } ?>
 <?php foreach ($new_products as $new_product) { ?>
@@ -133,6 +226,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         <h3><?= $new_product['name'] ?></h3>
         <p><?= $new_product['description'] ?></p>
         <div class="price">$<?= $new_product['price'] ?></div>
+        <button>delete</button>
     </div>
 <?php } ?>
        
@@ -143,7 +237,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             <input type="text" name="name" id="name" placeholder="Product Name" required>
             <input type="text" name="description" id="description" placeholder="Description" required>
             <input type="text" name="price" id="price" placeholder="Price" required>
-            <input type="file" value="Upload Image">
+            <input type="file" name="image" id="image" accept="image/*" required>
             <button type="submit" name="submit">Add Product</button>
         </form>
         <form action="index.php" method="post" enctype="multipart/form-data">
@@ -151,7 +245,8 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             <input type="text" name="name" id="name" placeholder="Product Name" required>
             <input type="text" name="description" id="description" placeholder="Description" required>
             <input type="text" name="price" id="price" placeholder="Price" required>
-            <input type="file" value="Upload Image">
+            <input type="file" name="image" id="image" accept="image/*" required>
+            <!-- Básicamente, limita la selección a archivos que el navegador reconoce como imágenes. -->
             <button type="submit" name="submit">Edit Product</button>
         </form>
     </section>
